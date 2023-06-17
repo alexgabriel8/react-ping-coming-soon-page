fa69d2016d1519211b75d4a06e3055a18be0b255
alexgabriel8 <alexgabriels8@outlook.com>
2023-06-17 20:15:24 -0300

    Updated regex of isEmailValid function and made more tests with it

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,45 @@
-import { render, screen } from '@testing-library/react';
 import { isEmailValid } from './scripts/is-email-valid';
 
-describe('Email Validation', () => {
-  it('Should consider "name@host.tld" valid', () => {
-    const validationResult = isEmailValid('name@host.tld')
-    expect(validationResult).toBe(true)
+describe('Validation of valid email:', () => {
+  it('Should consider "  name@host.tld  " with whitespace valid and return it trimmed', () => {
+    const email = '     name@host.tld      '
+    const validationResult = isEmailValid(email)
+    expect(validationResult).toBe('name@host.tld')
+  })
+})
+
+describe('Validation of invalid emails:', () => {
+  it('Should return "empty" when no argument is sent', () => {
+    const validationResult = isEmailValid()
+    expect(validationResult).toBe("empty")
+  })
+
+  it('Should return false for an email without at-sign', () => {
+    const email = 'namehost.tld'
+    const validationResult = isEmailValid(email)
+    expect(validationResult).toBe(false)
+  })
+
+  describe('Before at-sign validation:', () => {
+    it('Should return false for local not starting with number or letter', () => {
+      const email = '.name@host.tld'
+      const validationResult = isEmailValid(email)
+      expect(validationResult).toBe(false)
+    })
+
+    it('Should return false if local does not end with a word or number', () => {
+      const email = 'name.@host.tld'
+      const validationResult = isEmailValid(email)
+      expect(validationResult).toBe(false)
+    })
+  })
+
+  describe('After at-sign validation:', () => {
+    it('Should return false for an email without a TLD', () => {
+      const email = 'name@hosttld'
+      const validationResult = isEmailValid(email)
+      expect(validationResult).toBe(false)
+    })
+
   })
 })
--- src/scripts/is-email-valid.js
+++ src/scripts/is-email-valid.js
@@ -1,6 +1,6 @@
-const regex = /^([^.]+[^\.\@\-])+\@[^@.]+(\.[^@.][^@.]+)$/i
+const regex = /^[a-z0-9]+(\.[a-z0-9]+)?@[a-z0-9]+\.[a-z0-9]+$/i
 
-export function isEmailValid(rawEmail) {
+export function isEmailValid(rawEmail = '') {
     if (rawEmail.length === 0) return 'empty'
     const email = rawEmail.trim()
     if (email.match(regex) !== null) return email
